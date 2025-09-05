fa28b121c0ddb042b50661f00a1b538d6f7c955c
Mehmet-Aziz <gedikmehmetaziz@gmail.com>
2025-09-05 12:07:36 +0200

    Jour 1 : Generateur de message

---
+++ jour1.py
@@ -0,0 +1,20 @@
+#apprentssiage
+
+#print("Mon message")
+#print("MON nombre est : " , 4)
+#print("2+2 = ", 2+2)
+#name="Mehmet"
+#print(f"Salut , {name}")
+#nom = input("TON NOM ?")
+#print(f"SALUT {nom}")
+
+#generateur de message
+
+#demander le nom de l'utilisateur et son hobby
+name=input("QUelle est ton nom ?")
+hobby=input("Quelle est ton hobby ?")
+
+#generer le messager personnaliser
+print("\n BIENVENU")
+print(f"Bienvenu {name}")
+print(f"ton hobby est le {hobby} , c'est incroyable")
